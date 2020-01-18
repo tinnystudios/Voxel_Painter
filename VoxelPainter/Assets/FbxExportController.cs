cbabf7d23f5721d4f21a911664c7a988f097ddd7
tinnystudios <tinnystudios@gmail.com>
2020-01-18 20:27:00 +1100

    Add Fbx and Obj exporter --- Not completed

---
+++ VoxelPainter/Assets/FbxExportController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFBXExporter;
+using System.IO;
+
+public class FbxExportController : MonoBehaviour
+{
+    public GameObject TargetObject;
+
+    public void Export()
+    {
+        string path = Path.Combine(Application.persistentDataPath, "data");
+        path = Path.Combine(path, "carmodel" + ".fbx");
+
+        //Create Directory if it does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+        }
+
+        FBXExporter.ExportGameObjToFBX(TargetObject, path, true, true);
+    }
+}
---
+++ VoxelPainter/Assets/ObjExportController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class ObjExportController : MonoBehaviour 
+{
+    public GameObject TargetObject;
+
+    public void Export() 
+    {
+        string path = Path.Combine(Application.persistentDataPath, "data");
+        path = Path.Combine(path, "carmodel" + ".obj");
+
+        //Create Directory if it does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+        }
+
+        MeshFilter meshFilter = TargetObject.GetComponent<MeshFilter>();
+        ObjExporter.MeshToFile(meshFilter, path);
+    }
+}
